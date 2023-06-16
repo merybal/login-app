d8e0cfca376b0670b0178b62da7f5084418e6b44
Maria Baltar <merybaltar@gmail.com>
2023-06-16 16:31:11 -0300

    input component password

--- src/components/Common/Input.jsx
+++ src/components/Common/Input.jsx
@@ -1,6 +1,32 @@
 import styles from "components/Common/Input.module.scss";
 
+import { ReactComponent as EyeOpen } from "assets/EyeOpen.svg";
+import { ReactComponent as EyeClosed } from "assets/EyeClosed.svg";
+import { useState } from "react";
+
 const Input = (props) => {
+  const [inputType, setInputType] = useState(props.type);
+  const [inputValue, setInputValue] = useState("");
+
+  // const [eyeIconVisible, setEyeIconVisible] = useState(true);
+
+  // const [passwordType, setPasswordType] = useState("password");
+  // const [passwordInput, setPasswordInput] = useState("");
+
+  let eyeIconVisible = false;
+
+  if (props.type === "password") {
+    eyeIconVisible = true;
+  }
+
+  const togglePassword = () => {
+    if (inputType === "password") {
+      setInputType("text");
+      return;
+    }
+    setInputType("password");
+  };
+
   return (
     <div className={styles["input-container"]}>
       <label
@@ -10,15 +36,27 @@ const Input = (props) => {
       >
         {props.label} <span>*</span>
       </label>
-      <input
-        id={props.id}
-        //   className={`${!isNameValid && styles.invalid}`}
-        className={styles.input}
-        type={props.type}
-        placeholder={props.placeholder}
-        //   value={enteredName}
-        //   onChange={nameChangeHandler}
-      />
+      <div className={styles["field-container"]}>
+        <input
+          id={props.id}
+          //   className={`${!isNameValid && styles.invalid}`}
+          className={styles.input}
+          type={inputType}
+          placeholder={props.placeholder}
+          //   value={enteredName}
+          //   onChange={nameChangeHandler}
+        />
+
+        {eyeIconVisible && (
+          <EyeOpen
+            className={styles.eye}
+            alt="eye open to reveal password"
+            onClick={togglePassword}
+          />
+        )}
+        {/* <EyeClosed className={styles.eye} alt="eye closed to hide password" /> */}
+      </div>
+
       {/* AGREGAR OJITO
       https://medium.com/@miguelznunez/html-css-javascript-how-to-show-hide-password-using-the-eye-icon-27f033bf84ad
       https://codingstatus.com/show-and-hide-password-using-react-js/*/}
